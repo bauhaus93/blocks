1b63d930fcfd2187a3497459f807d9b6621f7b34
Jakob Fischer <JakobFischer93@gmail.com>
2018-04-06 03:00:28 +0200

    Vert shader now passes normals to frag shader
    Normal is used to draw up faces in other color to the structure of the untextured terrain better. Noticed, that the height noise creates artifacts on negative xy values

--- src/graphics/shader/FragmentShader.glsl
+++ src/graphics/shader/FragmentShader.glsl
@@ -1,9 +1,13 @@
 #version 330 core
 
+in vec3 normal;
 out vec3 color;
 
 //uniform sampler2D uniformTexture;
 
 void main() {
-    color = vec3(0.0f, 1.0f, 0.0f);
+    if (normal == vec3(0.0f, 0.0f, 1.0f))
+        color = vec3(0.0f, 1.0f, 0.0f);
+    else
+        color = vec3(1.0f, 0.0f, 0.0f);
 }
