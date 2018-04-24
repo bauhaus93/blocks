08a7e1411f533d0952b8dcf71997a7b98be5ade3
Jakob <jakobfischer93@gmail.com>
2018-04-24 21:10:23 +0200

    Got basic test system

---
+++ test/test_point/TestPoint.cpp
@@ -0,0 +1,22 @@
+#include "gtest/gtest.h"
+
+#include "utility/Point2.hpp"
+
+namespace mc {
+
+TEST(TestPoint, a) {
+    ASSERT_TRUE(true);
+}
+
+TEST(TestPoint, b) {
+    Point2i p(0);
+    ASSERT_TRUE(true);
+}
+
+TEST(TestPoint, c) {
+    Point2i p(3);
+    ASSERT_TRUE(true);
+}
+
+
+}   // namespace mc
